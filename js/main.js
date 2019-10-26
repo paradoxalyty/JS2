121630f8890cf605bd19ff2336f0abf8b641df74
paradoxalyty <paradoxalyty@gmail.com>
2019-10-26 16:11:05 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const products = [
+  {id: 1, title: 'Notebook', price: 20000},
+  {id: 2, title: 'Mouse', price: 1500},
+  {id: 3, title: 'Keyboard', price: 5000},
+  {id: 4, title: 'Gamepad', price: 4500},
+];
+
+const renderProduct = (title, price) => {
+  return `<div class="product-item">
+            <h3>${title}</h3>
+            <p>${price}</p>
+          </div>`;
+};
+
+const renderProducts = goodsList => {
+  document.querySelector('.products').innerHTML = goodsList.map(good => renderProduct(good.title, good.price)).join('');
+};
+
+renderProducts(products);
